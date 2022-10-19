31e93e93d6792663056e9d3e48bb4073c6e84f0c
localhostwiththemost <jpowelldev@gmail.com>
2022-10-19 15:42:51 -0500

    Refactored code to remove unnecessary function on page load

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("getscheme-btn")
-    .addEventListener("click", function () {
-      const seedColor = document.getElementById("seed-color").value;
-      const hexCode = seedColor.substring(1); // Removes # from hex code
-      const colorScheme = document.getElementById("select-scheme").value;
+const seedColor = document.getElementById("seed-color").value;
+const hexCode = seedColor.substring(1); // Removes # from hex code
+const colorScheme = document.getElementById("select-scheme").value;
 
-      fetch(
-        `https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${colorScheme}&count=5`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          console.log(data.colors[0].hex.value);
+document.getElementById("getscheme-btn").addEventListener("click", function () {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${colorScheme}&count=5`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.colors[0].hex.value);
 
-          document.getElementById("colors").innerHTML = `
+      document.getElementById("colors").innerHTML = `
           <div class="color-scheme">
           <div class="color" style="background-color:${data.colors[0].hex.value}"></div>
           <div class="color" style="background-color:${data.colors[1].hex.value}"></div>
@@ -26,15 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
           </div>
           `;
 
-          document.getElementById("hex-codes").innerHTML = `
+      document.getElementById("hex-codes").innerHTML = `
           <div class="hex-scheme">      
           <div class="hex"><p>${data.colors[0].hex.value}</p></div>
-                <div class="hex"><p>${data.colors[1].hex.value}</p></div>
-                <div class="hex"><p>${data.colors[2].hex.value}</p></div>
-                <div class="hex"><p>${data.colors[3].hex.value}</p></div>
-                <div class="hex"><p>${data.colors[4].hex.value}</p></div>
+                <div class="hex"><p class="copy-hex">${data.colors[1].hex.value}</p></div>
+                <div class="hex"><p class="copy-hex">${data.colors[2].hex.value}</p></div>
+                <div class="hex"><p class="copy-hex">${data.colors[3].hex.value}</p></div>
+                <div class="hex"><p class="copy-hex">${data.colors[4].hex.value}</p></div>
                </div>
                 `;
-        });
     });
 });
